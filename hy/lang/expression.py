8f20829b08ae4b78c041c75ab8c0025daa5eeedb
Paul Tagliamonte <paultag@debian.org>
2012-12-15 17:39:10 -0500

    adding in test for more stuff.

--- hy/lang/expression.py
+++ hy/lang/expression.py
@@ -1,3 +1,12 @@
 class HYExpression(list):
     def __init__(self, nodes):
         self += nodes
+
+    def get_invocation(self):
+        fn = self[0] if len(self) > 0 else ""
+        args = self[1:] if len(self) > 1 else []
+
+        return {
+            "function": fn,
+            "args": args
+        }
---
+++ tests/lang/test_expression.py
@@ -0,0 +1,24 @@
+from hy.lex.tokenize import tokenize
+from hy.lang.expression import HYExpression
+
+code = """
+(+ 1 1)  ; this is a test.
+(fn foo bar)  ; this is a test.
+"""
+
+
+def test_fn_split():
+    """Test if we can get a statement something right."""
+    one, two = tokenize(code)
+    assert one.get_invocation() == {
+        "function": "+",
+        "args": [
+            "1", "1"
+        ]
+    }
+    assert two.get_invocation() == {
+        "function": "fn",
+        "args": [
+            "foo", "bar"
+        ]
+    }
