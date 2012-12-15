11fe591d38c3b1fe749d27e7a4098650203a0947
Paul Tagliamonte <paultag@debian.org>
2012-12-15 18:24:06 -0500

    testing thinger

--- tests/lexer/test_list_lexing.py
+++ tests/lexer/test_list_lexing.py
@@ -6,3 +6,17 @@ def test_list_lex():
     assert fn == [
         "fn", ["1", "2", "3", "4"]
     ]
+
+
+def test_list_recurse():
+    fn = tokenize("(fn [1 2 3 4 [5 6 7]])")[0]
+    assert fn == [
+        "fn", ["1", "2", "3", "4", ["5", "6", "7"]]
+    ]
+
+
+def test_double_rainbow():
+    fn = tokenize("(fn [1 2 3 4] [5 6 7])")[0]
+    assert fn == [
+        "fn", ["1", "2", "3", "4"], ["5", "6", "7"]
+    ]
