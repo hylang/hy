925b9ec0a554b2c85a46cecc7e1bba03d1407b1e
Paul Tagliamonte <paultag@debian.org>
2012-12-15 14:48:16 -0500

    Initial import.

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+long_description = open('README.md', 'r').read()
+appname = "hy"
+version = "0.0.1"
+
+setup(**{
+    "name": appname,
+    "version": version,
+    "packages": [
+        'monomoy'
+    ],
+    "author": "Paul Tagliamonte",
+    "author_email": "paultag@debian.org",
+    "long_description": long_description,
+    "description": 'does some stuff with things & stuff',
+    "license": "Expat",
+    "url": "",
+    "platforms": ['any']
+})
