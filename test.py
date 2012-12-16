f36fa0f6de11773beb4f8ed339d938d538159327
Paul Tagliamonte <paultag@debian.org>
2012-12-16 16:40:27 -0500

    lolawesome.

--- test.py
+++ test.py
@@ -5,7 +5,10 @@ from hy.lex.tokenize import tokenize
 m = forge_module(
     'test',
     'test.hy',
-    tokenize('(def two (fn [] (print (+ 1 1))))')
+    tokenize('(def two (fn [] (print (+ 1 1))))(def x 1)')
 )
 
 print m.two
+m.two()
+
+print m.x
