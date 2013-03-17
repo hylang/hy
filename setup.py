d1ac469a3b46e4d336ddb942d2b32af0877b98b5
Paul R. Tagliamonte <paultag@debian.org>
2013-03-17 11:55:40 -0400

    adding in 2.6, we added it back in to the code

--- setup.py
+++ setup.py
@@ -56,6 +56,7 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Lisp",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
