1756e534add85178cd52abcd69952f249517cfc6
Nicolas Dandrimont <nicolas.dandrimont@crans.org>
2013-04-13 21:45:33 +0200

    The b prefix works since python2.6

--- tests/importer/test_pyc.py
+++ tests/importer/test_pyc.py
@@ -1,5 +1,4 @@
 import os
-import sys
 import imp
 import tempfile
 from hy.importer import write_hy_as_pyc
@@ -8,10 +7,7 @@ from hy.importer import write_hy_as_pyc
 def test_pyc():
     """Test pyc compilation."""
     f = tempfile.NamedTemporaryFile(suffix='.hy', delete=False)
-    if sys.version_info[0] >= 3:
-        f.write(b'(defn pyctest [s] s)')
-    else:
-        f.write('(defn pyctest [s] s)')
+    f.write(b'(defn pyctest [s] s)')
     f.close()
 
     write_hy_as_pyc(f.name)
