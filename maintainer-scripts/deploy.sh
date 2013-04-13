eb30c5857e07c24c0fca75874684258ed03d94f7
Paul R. Tagliamonte <paultag@debian.org>
2013-04-12 23:16:20 -0400

    Removing the website updater.

--- maintainer-scripts/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-metatron() {
-    ssh metatron.pault.ag $@
-}
-
-metatron "cd /srv/www/uwsgi/app/hy; git pull"
-metatron "cd /srv/www/uwsgi/app/hy/site; make"
-metatron -l www "kill-apps; start-apps"
