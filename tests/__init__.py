fa0a3c3945fe5b1e1b25cffc21ffb75e398f8188
Tuukka Turto <tuukka.turto@oktaeder.net>
2013-07-08 08:59:15 +0300

    missing import *

--- tests/__init__.py
+++ tests/__init__.py
@@ -10,3 +10,4 @@ from .native_tests.language import *  # noqa
 from .native_tests.unless import *  # noqa
 from .native_tests.when import *  # noqa
 from .native_tests.with_decorator import *  # noqa
+from .native_tests.setf import * # noqa
