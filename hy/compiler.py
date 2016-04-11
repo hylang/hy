b4aa66655cb279562b2827cf747b26422b154178
John Kitchin <jkitchin@andrew.cmu.edu>
2016-04-11 10:02:33 -0400

    add . docstring

--- hy/compiler.py
+++ hy/compiler.py
@@ -1515,6 +1515,29 @@ class HyASTCompiler(object):
     @builds(".")
     @checkargs(min=1)
     def compile_attribute_access(self, expr):
+        """. is used to perform attribute access on objects.
+
+        It uses a small DSL to allow quick access to attributes and items in a
+        nested data structure.
+
+        For instance,
+
+        (. foo bar baz [(+ 1 2)] frob)
+        Compiles down to:
+
+        foo.bar.baz[1 + 2].frob
+
+        . compiles its first argument (in the example, foo) as the object on
+        which to do the attribute dereference. It uses bare symbols as
+        attributes to access (in the example, bar, baz, frob), and compiles
+        the contents of lists (in the example, [(+ 1 2)]) for indexation.
+        Other arguments raise a compilation error.
+
+        Access to unknown attributes raises an AttributeError. Access to
+        unknown keys raises an IndexError (on lists and tuples) or a KeyError
+        (on dictionaries).
+
+        """
         expr.pop(0)  # dot
 
         ret = self.compile(expr.pop(0))
