32e76caafee4f8c44edfccea3a62e3ce5a74a73f
Kodi Arfer <git@arfer.net>
2017-03-17 09:31:54 -0700

    Refactor test_bin

--- tests/test_bin.py
+++ tests/test_bin.py
@@ -29,110 +29,87 @@ from hy._compat import PY3
 hy_dir = os.environ.get('HY_DIR', '')
 
 
-def run_cmd(cmd, stdin_data=None):
+def run_cmd(cmd, stdin_data=None, expect=0):
     p = subprocess.Popen(os.path.join(hy_dir, cmd),
                          stdin=subprocess.PIPE,
                          stdout=subprocess.PIPE,
                          stderr=subprocess.PIPE,
                          shell=True)
-    stdout = ""
-    stderr = ""
     if stdin_data is not None:
         p.stdin.write(stdin_data.encode('ASCII'))
         p.stdin.flush()
         p.stdin.close()
     # Read stdout and stderr otherwise if the PIPE buffer is full, we might
     # wait for ever…
+    stdout = ""
+    stderr = ""
     while p.poll() is None:
         stdout += p.stdout.read().decode('utf-8')
         stderr += p.stderr.read().decode('utf-8')
-    return p.returncode, stdout, stderr
+    assert p.returncode == expect
+    return stdout, stderr
 
 
 def test_bin_hy():
-    ret = run_cmd("hy", "")
-    assert ret[0] == 0
+    run_cmd("hy", "")
 
 
 def test_bin_hy_stdin():
-    ret = run_cmd("hy", '(koan)')
-    assert ret[0] == 0
-    assert "monk" in ret[1]
+    output, _ = run_cmd("hy", '(koan)')
+    assert "monk" in output
 
 
 def test_bin_hy_cmd():
-    ret = run_cmd("hy -c \"(koan)\"")
-    assert ret[0] == 0
-    assert "monk" in ret[1]
+    output, _ = run_cmd("hy -c \"(koan)\"")
+    assert "monk" in output
 
-    ret = run_cmd("hy -c \"(koan\"")
-    assert ret[0] == 1
-    assert "Premature end of input" in ret[2]
+    _, err = run_cmd("hy -c \"(koan\"", expect=1)
+    assert "Premature end of input" in err
 
 
 def test_bin_hy_icmd():
-    ret = run_cmd("hy -i \"(koan)\"", "(ideas)")
-    assert ret[0] == 0
-    output = ret[1]
-
+    output, _ = run_cmd("hy -i \"(koan)\"", "(ideas)")
     assert "monk" in output
     assert "figlet" in output
 
 
 def test_bin_hy_icmd_file():
-    ret = run_cmd("hy -i resources/icmd_test_file.hy", "(ideas)")
-    assert ret[0] == 0
-    output = ret[1]
-
+    output, _ = run_cmd("hy -i resources/icmd_test_file.hy", "(ideas)")
     assert "Hy!" in output
 
 
 def test_bin_hy_icmd_and_spy():
-    ret = run_cmd("hy -i \"(+ [] [])\" --spy", "(+ 1 1)")
-    assert ret[0] == 0
-    output = ret[1]
-
+    output, _ = run_cmd("hy -i \"(+ [] [])\" --spy", "(+ 1 1)")
     assert "([] + [])" in output
 
 
 def test_bin_hy_missing_file():
-    ret = run_cmd("hy foobarbaz")
-    assert ret[0] == 2
-    assert "No such file" in ret[2]
+    _, err = run_cmd("hy foobarbaz", expect=2)
+    assert "No such file" in err
 
 
 def test_bin_hy_file_with_args():
-    ret = run_cmd("hy tests/resources/argparse_ex.hy -h")
-    assert ret[0] == 0
-    assert "usage" in ret[1]
-    ret = run_cmd("hy tests/resources/argparse_ex.hy -c bar")
-    assert ret[0] == 0
-    assert "got c" in ret[1]
-    ret = run_cmd("hy tests/resources/argparse_ex.hy -i foo")
-    assert ret[0] == 0
-    assert "foo" in ret[1]
-    ret = run_cmd("hy tests/resources/argparse_ex.hy -i foo -c bar")
-    assert ret[0] == 0
-    assert "foo" in ret[1]
+    assert "usage" in run_cmd("hy tests/resources/argparse_ex.hy -h")[0]
+    assert "got c" in run_cmd("hy tests/resources/argparse_ex.hy -c bar")[0]
+    assert "foo" in run_cmd("hy tests/resources/argparse_ex.hy -i foo")[0]
+    assert "foo" in run_cmd("hy tests/resources/argparse_ex.hy -i foo -c bar")[0]  # noqa
 
 
 def test_bin_hyc():
-    ret = run_cmd("hyc")
-    assert ret[0] == 2
-    assert "usage" in ret[2]
-    ret = run_cmd("hyc -h")
-    assert ret[0] == 0
-    assert "usage" in ret[1]
-    ret = run_cmd("hyc tests/resources/argparse_ex.hy")
-    assert ret[0] == 0
-    assert "Compiling" in ret[1]
+    _, err = run_cmd("hyc", expect=2)
+    assert "usage" in err
+
+    output, _ = run_cmd("hyc -h")
+    assert "usage" in output
+
+    output, _ = run_cmd("hyc tests/resources/argparse_ex.hy")
+    assert "Compiling" in output
     assert os.path.exists("tests/resources/argparse_ex.pyc")
 
 
 def test_bin_hyc_missing_file():
-    ret = run_cmd("hyc foobarbaz")
-    assert ret[0] == 2
-    assert "[Errno 2]" in ret[2]
+    _, err = run_cmd("hyc foobarbaz", expect=2)
+    assert "[Errno 2]" in err
 
 
 def test_hy2py():
@@ -144,10 +121,10 @@ def test_hy2py():
                     continue
                 else:
                     i += 1
-                    ret = run_cmd("hy2py -s -a " + os.path.join(dirpath, f))
-                    assert ret[0] == 0, f
-                    assert len(ret[1]) > 1, f
-                    assert len(ret[2]) == 0, f
+                    output, err = run_cmd("hy2py -s -a " +
+                                          os.path.join(dirpath, f))
+                    assert len(output) > 1, f
+                    assert len(err) == 0, f
     assert i
 
 
@@ -159,48 +136,40 @@ def test_bin_hy_builtins():
 
 
 def test_bin_hy_main():
-    ret = run_cmd("hy tests/resources/bin/main.hy")
-    assert ret[0] == 0
-    assert "Hello World" in ret[1]
+    output, _ = run_cmd("hy tests/resources/bin/main.hy")
+    assert "Hello World" in output
 
 
 def test_bin_hy_main_args():
-    ret = run_cmd("hy tests/resources/bin/main.hy test 123")
-    assert ret[0] == 0
-    assert "test" in ret[1]
-    assert "123" in ret[1]
+    output, _ = run_cmd("hy tests/resources/bin/main.hy test 123")
+    assert "test" in output
+    assert "123" in output
 
 
 def test_bin_hy_main_exitvalue():
-    ret = run_cmd("hy tests/resources/bin/main.hy exit1")
-    assert ret[0] == 1
+    run_cmd("hy tests/resources/bin/main.hy exit1", expect=1)
 
 
 def test_bin_hy_no_main():
-    ret = run_cmd("hy tests/resources/bin/nomain.hy")
-    assert ret[0] == 0
-    assert "This Should Still Work" in ret[1]
+    output, _ = run_cmd("hy tests/resources/bin/nomain.hy")
+    assert "This Should Still Work" in output
 
 
 def test_bin_hy_module_main():
-    ret = run_cmd("hy -m tests.resources.bin.main")
-    assert ret[0] == 0
-    assert "Hello World" in ret[1]
+    output, _ = run_cmd("hy -m tests.resources.bin.main")
+    assert "Hello World" in output
 
 
 def test_bin_hy_module_main_args():
-    ret = run_cmd("hy -m tests.resources.bin.main test 123")
-    assert ret[0] == 0
-    assert "test" in ret[1]
-    assert "123" in ret[1]
+    output, _ = run_cmd("hy -m tests.resources.bin.main test 123")
+    assert "test" in output
+    assert "123" in output
 
 
 def test_bin_hy_module_main_exitvalue():
-    ret = run_cmd("hy -m tests.resources.bin.main exit1")
-    assert ret[0] == 1
+    run_cmd("hy -m tests.resources.bin.main exit1", expect=1)
 
 
 def test_bin_hy_module_no_main():
-    ret = run_cmd("hy -m tests.resources.bin.nomain")
-    assert ret[0] == 0
-    assert "This Should Still Work" in ret[1]
+    output, _ = run_cmd("hy -m tests.resources.bin.nomain")
+    assert "This Should Still Work" in output
