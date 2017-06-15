032830bada6f0670bd929d006e112ed2d8431874
Kodi Arfer <git@arfer.net>
2017-06-15 11:11:54 -0700

    Mark support for Python 3.6 in setup.py

--- setup.py
+++ setup.py
@@ -78,6 +78,7 @@ setup(
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "Topic :: Software Development :: Code Generators",
         "Topic :: Software Development :: Compilers",
         "Topic :: Software Development :: Libraries",
