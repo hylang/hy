20516cce7946041174e770e6678ddc3efe6d0f79
gilch <gilch@users.noreply.github.com>
2017-08-30 23:36:06 -0600

    make native tests run on Windows too

--- conftest.py
+++ conftest.py
@@ -1,10 +1,13 @@
 import _pytest
 import hy
+import os
 from hy._compat import PY3, PY35
+
+NATIVE_TESTS = os.path.join("", "tests", "native_tests", "")
 
 def pytest_collect_file(parent, path):
     if (path.ext == ".hy"
-            and "/tests/native_tests/" in path.dirname + "/"
+            and NATIVE_TESTS in path.dirname + os.sep
             and path.basename != "__init__.hy"
             and not ("py3_only" in path.basename and not PY3)
             and not ("py35_only" in path.basename and not PY35)):
