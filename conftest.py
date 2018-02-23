3ad30204857a1cf50c4f4a8a811611e35a70d084
Simon Gomizelj <simon@vodik.xyz>
2018-02-23 17:39:44 -0500

    Ignore py3*_only tests everywhere

--- conftest.py
+++ conftest.py
@@ -1,15 +1,20 @@
 import pytest
-import hy
+import hy  # NOQA
 import os
 from hy._compat import PY3, PY35, PY36
 
+
 NATIVE_TESTS = os.path.join("", "tests", "native_tests", "")
+
+
+def pytest_ignore_collect(path, config):
+    return (("py3_only" in path.basename and not PY3)
+            or ("py35_only" in path.basename and not PY35)
+            or ("py36_only" in path.basename and not PY36))
+
 
 def pytest_collect_file(parent, path):
     if (path.ext == ".hy"
-            and NATIVE_TESTS in path.dirname + os.sep
-            and path.basename != "__init__.hy"
-            and not ("py3_only" in path.basename and not PY3)
-            and not ("py35_only" in path.basename and not PY35)
-            and not ("py36_only" in path.basename and not PY36)):
+        and NATIVE_TESTS in path.dirname + os.sep
+        and path.basename != "__init__.hy"):
         return pytest.Module(path, parent)
