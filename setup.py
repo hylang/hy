21f13b977dfa160a18fb7856b2fb63eeec1b16e3
Kodi Arfer <git@arfer.net>
2018-03-24 12:39:54 -0700

    Update NEWS and classifiers for Python 3.7 support

--- setup.py
+++ setup.py
@@ -84,6 +84,7 @@ setup(
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Topic :: Software Development :: Code Generators",
         "Topic :: Software Development :: Compilers",
         "Topic :: Software Development :: Libraries",
