ec8138dd4644c1e46ac9934d7f858f089e92d9ed
Kodi Arfer <git@arfer.net>
2018-03-26 14:32:08 -0700

    Update test_bin

--- tests/test_bin.py
+++ tests/test_bin.py
@@ -19,8 +19,8 @@ from hy.importer import get_bytecode_path
 hy_dir = os.environ.get('HY_DIR', '')
 
 
-def hr(s=""):
-    return "hy --repl-output-fn=hy.contrib.hy-repr.hy-repr " + s
+def pyrepr(s=""):
+    return "hy --repl-output-fn=repr " + s
 
 
 def run_cmd(cmd, stdin_data=None, expect=0, dontwritebytecode=False):
@@ -73,7 +73,7 @@ def test_bin_hy_stdin():
 
 def test_bin_hy_stdin_multiline():
     output, _ = run_cmd("hy", '(+ "a" "b"\n"c" "d")')
-    assert "'abcd'" in output
+    assert '"abcd"' in output
 
 
 def test_bin_hy_history():
@@ -81,18 +81,18 @@ def test_bin_hy_history():
                                  (+ "c" "d")
                                  (+ "e" "f")
                                  (.format "*1: {}, *2: {}, *3: {}," *1 *2 *3)''')
-    assert "'*1: ef, *2: cd, *3: ab,'" in output
+    assert '"*1: ef, *2: cd, *3: ab,"' in output
 
     output, _ = run_cmd("hy", '''(raise (Exception "TEST ERROR"))
                                  (+ "err: " (str *e))''')
-    assert "'err: TEST ERROR'" in output
+    assert '"err: TEST ERROR"' in output
 
 
 def test_bin_hy_stdin_comments():
     _, err_empty = run_cmd("hy", '')
 
     output, err = run_cmd("hy", '(+ "a" "b") ; "c"')
-    assert "'ab'" in output
+    assert '"ab"' in output
     assert err == err_empty
 
     _, err = run_cmd("hy", '; 1')
@@ -117,7 +117,7 @@ def test_bin_hy_stdin_assignment():
 def test_bin_hy_stdin_as_arrow():
     # https://github.com/hylang/hy/issues/1255
     output, _ = run_cmd("hy", "(as-> 0 it (inc it) (inc it))")
-    assert re.match(r"=>\s+2L?\s+=>", output)
+    assert re.match(r"=>\s+2\s+=>", output)
 
 
 def test_bin_hy_stdin_error_underline_alignment():
@@ -162,19 +162,19 @@ def test_bin_hy_stdin_bad_repr():
     assert "AZ" in output
 
 
-def test_bin_hy_stdin_hy_repr():
+def test_bin_hy_stdin_py_repr():
     output, _ = run_cmd("hy", '(+ [1] [2])')
+    assert "[1 2]" in output
+
+    output, _ = run_cmd(pyrepr(), '(+ [1] [2])')
     assert "[1, 2]" in output.replace('L', '')
 
-    output, _ = run_cmd(hr(), '(+ [1] [2])')
-    assert "[1 2]" in output
-
-    output, _ = run_cmd(hr("--spy"), '(+ [1] [2])')
+    output, _ = run_cmd(pyrepr("--spy"), '(+ [1] [2])')
     assert "[1]+[2]" in output.replace('L', '').replace(' ', '')
-    assert "[1 2]" in output
+    assert "[1, 2]" in output.replace('L', '')
 
     # --spy should work even when an exception is thrown
-    output, _ = run_cmd(hr("--spy"), '(+ [1] [2] (foof))')
+    output, _ = run_cmd(pyrepr("--spy"), '(+ [1] [2] (foof))')
     assert "[1]+[2]" in output.replace('L', '').replace(' ', '')
 
 def test_bin_hy_ignore_python_env():
