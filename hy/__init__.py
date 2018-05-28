f100904669a50a3fba718ee054fa8529192fe768
Simon Gomizelj <simon@vodik.xyz>
2018-05-27 22:59:09 -0400

    Add hy.interact

--- hy/__init__.py
+++ hy/__init__.py
@@ -14,3 +14,16 @@ import hy.importer  # NOQA
 
 from hy.core.language import read, read_str, mangle, unmangle  # NOQA
 from hy.importer import hy_eval as eval  # NOQA
+
+
+def interact(banner=None, readfunc=None, local=None, exitmsg=None):
+    from hy.cmdline import HyREPL
+    console = HyREPL(locals=local)
+    if readfunc is not None:
+        console.raw_input = readfunc
+    else:
+        try:
+            import readline
+        except ImportError:
+            pass
+    console.interact(banner, exitmsg)
