14979edcab424d75005d93ce7d5811ba21abbd26
Kodi Arfer <git@arfer.net>
2018-06-07 12:20:23 -0700

    Remove tests of the old comprehension forms

--- tests/compilers/test_ast.py
+++ tests/compilers/test_ast.py
@@ -554,14 +554,6 @@ def test_attribute_empty():
     cant_compile('[2].foo')
 
 
-def test_invalid_list_comprehension():
-    """Ensure that invalid list comprehensions do not break the compiler"""
-    cant_compile("(genexpr x [])")
-    cant_compile("(genexpr [x [1 2 3 4]] x)")
-    cant_compile("(list-comp None [])")
-    cant_compile("(list-comp [x [1 2 3]] x)")
-
-
 def test_bad_setv():
     """Ensure setv handles error cases"""
     cant_compile("(setv (a b) [1 2])")
