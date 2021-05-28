dcc94fc8b795925d7327e75e1d19f2ea64dcd2c4
Sunjay Cauligi <scauligi@eng.ucsd.edu>
2021-05-27 23:43:12 -0700

    Running hy module runs hy_main

--- hy/__main__.py
+++ hy/__main__.py
@@ -1,14 +1,8 @@
-import hy  # NOQA
 import sys
 
-# This just mocks the normalish behavior of the Python interp. Helpful to aid
-# with shimming existing apps that don't really "work" with Hy.
-#
-# You could say this script helps Hyjack a file.
-#
+from hy.cmdline import hy_main
 
+# Running hy as a module (e.g. `python -m hy`)
+# is equivalent to running the main `hy` command.
 
-if len(sys.argv) > 1:
-    sys.argv.pop(0)
-    hy.importer._import_from_path('__main__', sys.argv[0])
-    sys.exit(0)  # right?
+sys.exit(hy_main())
