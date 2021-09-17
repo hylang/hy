08bb2826c19155fcdee069617d2182a5b181354b
Kodi Arfer <git@arfer.net>
2021-09-17 16:32:20 -0400

    Use `oneplus` instead of `times(1, Inf, …)`
    This now works with the new `funcparserlib`.

--- hy/core/result_macros.py
+++ hy/core/result_macros.py
@@ -805,7 +805,7 @@ def compile_break_or_continue_expression(compiler, expr, root):
 # ------------------------------------------------
 
 @pattern_macro(["with", "with/a"], [
-    brackets(times(1, Inf, FORM + FORM)) |
+    brackets(oneplus(FORM + FORM)) |
         brackets(FORM >> (lambda x: [(Symbol('_'), x)])),
     many(FORM)])
 def compile_with_expression(compiler, expr, root, args, body):
