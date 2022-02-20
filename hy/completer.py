3012d3d6b00f80bd78cb6f573979c23eec3750d5
Joseph LaFreniere <joseph@lafreniere.xyz>
2022-02-20 00:33:42 -0600

    Add type hints to `hy.completer`

--- hy/completer.py
+++ hy/completer.py
@@ -1,17 +1,22 @@
+from __future__ import annotations
+
 import builtins
 import contextlib
 import os
 import re
 import sys
-from types import ModuleType
-from typing import Optional
+from typing import TYPE_CHECKING
 
 import hy.compiler
 import hy.macros
 
+if TYPE_CHECKING:
+    import typing as T
+    from types import ModuleType
+
 # Lazily import `readline` to work around
 # https://bugs.python.org/issue2675#msg265564
-readline: Optional[ModuleType] = None
+readline: T.Optional[ModuleType] = None
 
 
 def init_readline():
@@ -27,7 +32,10 @@ def init_readline():
 
 
 class Completer:
-    def __init__(self, namespace={}):
+    namespace: dict
+    path: list[dict[str, T.Any]]
+
+    def __init__(self, namespace: dict = {}):
         if not isinstance(namespace, dict):
             raise TypeError("namespace must be a dictionary")
         self.namespace = namespace
@@ -37,7 +45,7 @@ class Completer:
 
         self.path.append(namespace["__macros__"])
 
-    def attr_matches(self, text):
+    def attr_matches(self, text: str) -> list[str]:
         # Borrowed from IPython's completer
         m = re.match(r"(\S+(\.[\w-]+)*)\.([\w-]*)$", text)
 
@@ -55,7 +63,7 @@ class Completer:
             return []
 
         n = len(attr)
-        matches = []
+        matches: list[str] = []
         for w in words:
             if w[:n] == attr:
                 matches.append(
@@ -73,7 +81,7 @@ class Completer:
                         matches.append(k)
         return matches
 
-    def complete(self, text, state):
+    def complete(self, text: str, state: int) -> T.Optional[str]:
         if "." in text:
             matches = self.attr_matches(text)
         else:
@@ -85,7 +93,7 @@ class Completer:
 
 
 @contextlib.contextmanager
-def completion(completer=None):
+def completion(completer: T.Optional[Completer] = None):
     delims = "()[]{} "
 
     init_readline()
@@ -97,7 +105,7 @@ def completion(completer=None):
     if not completer:
         completer = Completer()
 
-    if sys.platform == "darwin" and "libedit" in readline.__doc__:
+    if sys.platform == "darwin" and "libedit" in getattr(readline, "__doc__"):
         readline_bind = "bind ^I rl_complete"
     else:
         readline_bind = "tab: complete"
