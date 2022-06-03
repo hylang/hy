hylang/hy

commit 049dcd7d9e2fbca1145ecde5a2e2c83266ff5130

Don't use `hy.compile` to bring `hy` into scope
This change might yield a tiny speedup, but it's more for the sake of simplicity.

hy/compiler.py
@@ -8,6 +8,7 @@ import types
 
 from funcparserlib.parser import NoParseError, many
 
+import hy
 from hy._compat import PY3_8
 from hy.errors import HyCompileError, HyLanguageError, HySyntaxError
 from hy.lex import mangle
@@ -345,6 +346,8 @@ class HyASTCompiler:
             self.module = importlib.import_module(module)
         else:
             self.module = module
+        self.module.hy = hy
+        # The `hy` module itself should always be in scope.
 
         self.module_name = self.module.__name__
 
@@ -863,12 +866,6 @@ def hy_compile(
 
     compiler = compiler or HyASTCompiler(module, filename=filename, source=source)
 
-    if import_stdlib:
-        # Import hy for compile time, but save the compiled AST.
-        stdlib_ast = compiler.compile(
-            mkexpr("eval-and-compile", mkexpr("import", "hy"))
-        )
-
     with compiler.scope:
         result = compiler.compile(tree)
     expr = result.force_expr
@@ -899,7 +896,8 @@ def hy_compile(
 
         # Import hy for runtime.
         if import_stdlib:
-            body += stdlib_ast.stmts
+            body.append(ast.fix_missing_locations(
+                ast.Import([ast.alias("hy", "hy")])))
 
     body += result.stmts
     ret = root(body=body, type_ignores=[])

tests/compilers/test_ast.py
@@ -639,9 +639,9 @@ def test_module_prelude():
     hy_ast = can_compile("", import_stdlib=True)
     assert len(hy_ast.body) == 1
     assert isinstance(hy_ast.body[0], ast.Import)
-    assert hy_ast.body[0].module == "hy"
+    assert hy_ast.body[0].names[0].name == "hy"
 
     hy_ast = can_compile("(setv flag (- hy.models.Symbol 1))", import_stdlib=True)
     assert len(hy_ast.body) == 2
     assert isinstance(hy_ast.body[0], ast.Import)
-    assert hy_ast.body[0].module == "hy"
+    assert hy_ast.body[0].names[0].name == "hy"

commit f0d07060adc412f42021932ebb5956a28b3db646

Don't redundantly `import hy as hy`
This was a regression introduced by 049dcd7d9e2fbca1145ecde5a2e2c83266ff5130.

hy/compiler.py
@@ -896,7 +896,7 @@ def hy_compile(
 
         # Import hy for runtime.
         if import_stdlib:
-            body.append(ast.fix_missing_locations(ast.Import([ast.alias("hy", "hy")])))
+            body.append(ast.fix_missing_locations(ast.Import([ast.alias("hy", None)])))
 
     body += result.stmts
     ret = root(body=body, type_ignores=[])

tests/compilers/test_ast.py
@@ -640,4 +640,6 @@ def test_module_prelude():
         x = can_compile(code, import_stdlib=True).body
         assert len(x) == n
         assert isinstance(x[0], ast.Import)
-        assert x[0].names[0].name == "hy"
+        x = x[0].names[0]
+        assert x.name == "hy"
+        assert x.asname is None