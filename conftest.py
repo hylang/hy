1937c99388fbdb59bec53d563e2d4ef2460c6e25
Kodi Arfer <git@arfer.net>
2023-01-12 15:14:33 -0500

    Remove obsolete `pytest_ignore_collect`

--- conftest.py
+++ conftest.py
@@ -1,36 +1,13 @@
 import importlib
 import os
-import sys
-from functools import reduce
-from operator import or_
 from pathlib import Path
 
 import pytest
-
-import hy
-from hy._compat import PY3_8, PY3_10, PY3_11
 
 NATIVE_TESTS = os.path.join("", "tests", "native_tests", "")
 
 # https://github.com/hylang/hy/issues/2029
 os.environ.pop("HYSTARTUP", None)
-
-
-def pytest_ignore_collect(path, config):
-    versions = [
-        (sys.version_info < (3, 8), "sub_py3_7_only"),
-        (PY3_8, "py3_8_only"),
-        (PY3_10, "py3_10_only"),
-        (PY3_11, "py3_11_only"),
-    ]
-
-    return (
-        reduce(
-            or_,
-            (name in path.basename and not condition for condition, name in versions),
-        )
-        or None
-    )
 
 
 def pytest_collect_file(parent, path):
