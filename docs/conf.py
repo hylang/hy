eda43687b809995cfa46b551b62f5b8f6a2780a2
Kodi Arfer <git@arfer.net>
2024-04-10 09:31:41 -0400

    Clean up and reorganize Sphinx configuration

--- docs/conf.py
+++ docs/conf.py
@@ -1,19 +1,14 @@
-# This file is execfile()d with the current directory set to its containing dir.
+import os, re, sys, time, html
 
-import html
-import os
-import re
-import sys
-import time
+sys.path.insert(0, os.path.abspath('..'))
 
-sys.path.insert(0, os.path.abspath(".."))
+import hy; hy.I = type(hy.I)  # A trick to enable `hy:autoclass:: hy.I`
 
 extensions = [
-    "sphinx.ext.napoleon",
-    "sphinx.ext.intersphinx",
-    "sphinx.ext.autodoc",
-    "sphinxcontrib.hydomain",
-]
+    'sphinx.ext.napoleon',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.autodoc',
+    'sphinxcontrib.hydomain']
 
 import warnings; import sphinx.deprecation as SD
 for c in (SD.RemovedInSphinx60Warning, SD.RemovedInSphinx70Warning):
@@ -21,49 +16,33 @@ for c in (SD.RemovedInSphinx60Warning, SD.RemovedInSphinx70Warning):
 
 from get_version import __version__ as hy_version
 
-source_suffix = ".rst"
-
-master_doc = "index"
+project = 'Hy'
+copyright = '%s the authors' % time.strftime('%Y')
 html_title = f'Hy {hy_version} manual'
-
-# General information about the project.
-project = "Hy"
-copyright = "%s the authors" % time.strftime("%Y")
-
-# The version info for the project you're documenting, acts as replacement for
-# |version| and |release|, also used in various other places throughout the
-# built documents.
-#
-# The short X.Y version.
-version = ".".join(hy_version.split(".")[:-1])
-# The full version, including alpha/beta/rc tags.
+version = '.'.join(hy_version.split('.')[:-1])
+  # The short dotted version identifier
 release = hy_version
+  # The full version identifier, including alpha, beta, and RC tags
 
-exclude_patterns = ["_build", "coreteam.rst"]
-add_module_names = True
+source_suffix = '.rst'
+master_doc = 'index'
+exclude_patterns = ['_build', 'coreteam.rst']
 
-html_theme = "nature"
+html_theme = 'nature'
 html_theme_options = dict(
     nosidebar = True,
     body_min_width = 0,
     body_max_width = 'none')
 html_css_files = ['custom.css']
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
-
+html_static_path = ['_static']
 html_use_smartypants = False
 html_copy_source = False
 html_show_sphinx = False
 
-highlight_language = "hylang"
+add_module_names = True
+
+highlight_language = 'hylang'
 
 intersphinx_mapping = dict(
-    py=("https://docs.python.org/3/", None),
-    hyrule=("https://hyrule.readthedocs.io/en/master/", None),
-)
-
-import hy
-hy.I = type(hy.I)  # A trick to enable `hy:autoclass:: hy.I`
+    py = ('https://docs.python.org/3/', None),
+    hyrule = ('https://hyrule.readthedocs.io/en/master/', None))
