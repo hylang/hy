a5e5d170dbc55eb66c51e82ecc6e984e733452a6
Kodi Arfer <git@arfer.net>
2026-01-14 12:32:28 -0500

    Bump `last_version`

--- hy/__init__.py
+++ hy/__init__.py
@@ -1,6 +1,6 @@
 __version__ = 'unreleased'
 nickname = None
-last_version = '1.1.0'
+last_version = '1.2.0'
   # This is used by `(pragma :hy …)` to guess whether an unreleased
   # version of Hy is new enough. In a released version, it's simply
   # equal to `__version__`.
