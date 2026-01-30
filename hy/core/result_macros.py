2bd43de2c3e21da8b1821d8d8a892a9a771a79bb
Sunjay Cauligi <scauligi@gmail.com>
2026-01-30 17:01:42 -0500

    HyReader param bracketed_templates and corresponding pragma

--- hy/core/result_macros.py
+++ hy/core/result_macros.py
@@ -58,7 +58,7 @@ from hy.models import (
     as_model,
     is_unpack,
 )
-from hy.reader import mangle
+from hy.reader import mangle, HyReader
 from hy.scoping import OuterVar, ScopeFn, ScopeGen, ScopeLet, is_function_scope, is_inside_function_scope, nearest_python_scope
 
 # ------------------------------------------------
@@ -185,6 +185,11 @@ def compile_pragma(compiler, expr, root, kwargs):
         elif kw == Keyword("warn-on-core-shadow"):
             compiler.local_state_stack[-1]['warn_on_core_shadow'] = (
                 bool(compiler.eval(value)))
+
+        elif kw == Keyword("bracketed-templates"):
+            reader = HyReader.current_reader(create=False)
+            if reader:
+                reader.bracketed_templates = bool(compiler.eval(value))
 
         else:
             raise compiler._syntax_error(kw, f"Unknown pragma `{kw}`. Perhaps it's implemented by a newer version of Hy.")
--- hy/reader/hy_reader.py
+++ hy/reader/hy_reader.py
@@ -126,8 +126,10 @@ class HyReader(Reader):
     NON_IDENT = set("()[]{};\"'`~")
     _current_reader = None
 
-    def __init__(self, *, use_current_readers=False):
+    def __init__(self, *, use_current_readers=False, bracketed_templates=False):
         super().__init__()
+
+        self.bracketed_templates = bracketed_templates
 
         # move any reader macros declared using
         # `reader_for("#...")` to the macro table
@@ -429,9 +431,11 @@ class HyReader(Reader):
         delim = "".join(delim)
         fstring_mode = (
             "f" if delim == "f" or delim.startswith("f-")
-            else "t" if PY3_14 and (delim == "t" or delim.startswith("t-"))
+            else "t" if self.bracketed_templates and (delim == "t" or delim.startswith("t-"))
             else ""
         )
+        if not PY3_14 and fstring_mode == "t":
+            raise LexException.from_reader("Template string literals require Python 3.14 or later", self)
 
         # discard single initial newline, if any, accounting for all
         # three styles of newline
